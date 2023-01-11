c314ee76131b0813d190e4e50aa3312e467c07ef
jonadb97 <jonydb97@gmail.com>
2023-01-11 10:48:01 -0300

    Añadida librería UI ElementUI

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,13 @@ import { createPinia, PiniaVuePlugin } from 'pinia'
 import { compileToFunctions } from 'vue-template-compiler'
 import Index from './components/index.vue'
 import 'animate.css'
-
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
-
 import VueRouter from 'vue-router'
-
 import './assets/main.css'
-
 import 'uno.css'
+
 // Routes
 
 
@@ -28,6 +27,7 @@ const router = new VueRouter({
 
 Vue.use(PiniaVuePlugin)
 Vue.use(VueRouter)
+Vue.use(ElementUI)
 
 
 // Start Vue
