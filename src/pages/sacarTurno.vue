df2a04d440b89bbcc1a320d68c1e427aa47099a5
jonadb97 <jonydb97@gmail.com>
2023-01-17 10:15:19 -0300

    Imports 10

--- src/pages/sacarTurno.vue
+++ src/pages/sacarTurno.vue
@@ -3,24 +3,24 @@
         <div id="turnos-section1" class="h-screen">
             <h1>Sacar turno</h1>
             <h2 class="mt-6">¿Qué te vas a hacer?</h2>
-            <el-carousel class="w-200 ml-auto mr-auto mt-50" :interval="4000" type="card" height="200px">
+            <el-carousel class="w-200 ml-auto mr-auto mt-20" :interval="4000" type="card" height="200px">
                 <el-carousel-item v-for="item in 6" :key="item">
                     <h3 class="medium">{{ item }}</h3>
                 </el-carousel-item>
             </el-carousel>
-            <a href="#turnos-section2"><el-button class="mt-50" icon="el-icon-arrow-down" circle></el-button></a>
+            <a href="#turnos-section2"><el-button class="mt-20" icon="el-icon-arrow-down" circle></el-button></a>
         </div>
         <div id="turnos-section2" class="h-screen">
-            <h2 class="pt-50">¿Con quién?</h2>
-            <el-carousel class="w-200 ml-auto mr-auto mt-50" :interval="4000" type="card" height="200px">
+            <h2 class="pt-20">¿Con quién?</h2>
+            <el-carousel class="w-200 ml-auto mr-auto mt-20" :interval="4000" type="card" height="200px">
                 <el-carousel-item v-for="item in 6" :key="item">
                     <h3 class="medium">{{ item }}</h3>
                 </el-carousel-item>
             </el-carousel>
-            <a href="#turnos-section3"><el-button class="mt-50" icon="el-icon-arrow-down" circle></el-button></a>
+            <a href="#turnos-section3"><el-button class="mt-20" icon="el-icon-arrow-down" circle></el-button></a>
         </div>
         <div id="turnos-section3" class="h-screen">
-            <h2 class="pt-50">¿Cuando?</h2>
+            <h2 class="pt-20">¿Cuando?</h2>
             <div>
               <!--  <div class="flex mb-2">
                     <label class="text-gray-600 font-medium text-center m-auto"><input class="mr-1" type="radio" value=""
@@ -32,7 +32,7 @@
             </div>
 
             <br>
-            <el-button class="mt-50" @click="$router.push('/turnosPendientes')" icon="el-icon-check" type="success" circle></el-button>
+            <el-button class="" @click="$router.push('/turnosPendientes')" icon="el-icon-check" type="success" circle></el-button>
         </div>
     </div>
 </template>
