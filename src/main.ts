38e25d4d4a8e8c171c1c94daf4e3c16f07d8b498
jonadb97 <jonydb97@gmail.com>
2023-01-17 09:47:22 -0300

    Imports 5

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import SacarTurno from './pages/sacarTurno.vue'
 import TurnosPendientes from './pages/turnosPendientes.vue'
 import 'animate.css'
 import ElementUI from 'element-ui'
-import VCalendar from 'v-calendar'
+import {VCalendar} from 'v-calendar'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import VueRouter from 'vue-router'
@@ -38,7 +38,7 @@ const router = new VueRouter({
 
 Vue.use(PiniaVuePlugin)
 Vue.use(VueRouter)
-Vue.use(ElementUI, { locale })
+Vue.use(ElementUI)
 Vue.use(VCalendar)
 
 
